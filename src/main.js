7495e4af2542596992279e0c826e50a6708f50ac
Justin Quan <46581109+JustinAQuan@users.noreply.github.com>
2021-04-21 15:45:05 -0700

    added header to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
+/********************************************************************
+Collaborator Names: 
+    Justin Quan
+    Michelle Lytle
+    Thea Knasiak
+
+Game Title:
+    Untitled
+
+Date Completed:
+    TBD
+
+Creative Tilt Justification:
+1)  do something technically interesting?
+    Are you particularly proud of a programming technique you implemented?
+    Did you look beyond the class examples and learn how to do something new? (5)
+
+2)  ...have a great visual style?
+    Does it use music or art that you're particularly proud of?
+    Are you trying something new or clever with the endless runner form? (5)
+********************************************************************/
+
 let config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 1200,
     height: 600,
     scale: {
         parent: 'phaser-example',
