32253b71fe287cb911b36d4e846076a97f0d719b
Justin Quan <46581109+JustinAQuan@users.noreply.github.com>
2021-04-21 12:15:03 -0700

    created a player with temporary sprite

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,10 +8,10 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.Player = new Rectangle(this, game.config.width/2, game.config.height/2, 50, 50, 0x666666).setOrigin(0, 0);
+        this.add.text(game.config.width / 2, game.config.height / 2, 'GAME MENU');
     }
 
     update() {
-
+        this.scene.start('playScene');
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,14 +4,19 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        
+        this.load.image('Player', './assets/player.png');
+        this.load.image('Floor', './assets/floor.png');
     }
 
     create() {
-
+        // adds floor
+        this.floor = this.add.tileSprite(0, game.config.height - game.config.height / 5, game.config.width, game.config.height / 5, 'Floor').setOrigin(0, 0);
+        
+        // adds player sprite
+        this.Player = new Player(this, game.config.width / 5, game.config.height - game.config.height / 5, 'Player', 0);
     }
 
     update() {
-        
+        this.floor.tilePositionX += 2;
     }
 }
