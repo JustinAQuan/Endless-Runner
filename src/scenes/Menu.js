d3be6b370857abaf922a1feed92a02c656856ab3
Michelle Lytle <mnlytle@ucsc.edu>
2021-04-28 16:06:41 -0700

    added menu art and music

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,14 +4,42 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
+        // menu background music 
+        this.load.audio(
+            'menu_bgm', 
+            './assets/menu_bgm.wav'
+        );
+
+        // menu image 
+        this.load.image(
+            'corgiMenuArt', 
+            './assets/corgiMenuArt.png'
+        );
         
     }
 
     create() {
+        this.menu_bgm = this.sound.add(
+            'menu_bgm', 
+            {
+                volume: 1,
+                loop: true
+            }
+        );
+
+        // plays space background music
+        this.menu_bgm.play();
+
+        this.corgiMenuArt = this.add.image(
+            0, 
+            0, 
+            'corgiMenuArt'
+        ).setOrigin(0, 0);
+
         this.add.text(game.config.width / 2, game.config.height / 2, 'GAME MENU').setOrigin(0.5, 0);
 
         this.time.delayedCall(1000, () => {
-            this.scene.start('playScene', {highscore: 0});
+            this.menu_bgm.stop(); this.scene.start('playScene', {highscore: 0});
         }, null, this);
     }
 }
